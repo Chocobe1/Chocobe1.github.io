f8d66ac093999b10c070adeee9a6d1cbd9e57a67
Chocobe1 <46614466+Chocobe1@users.noreply.github.com>
2023-11-20 11:01:26 +0000

    Deploying to gh-pages from @ Chocobe1/Chocobe1.github.io@75419f8c611b9ac7f5a96b32e3fced63e3e3165d 🚀

--- _next/static/chunks/app/my-mdx/page-af1b3ed0b41d9ab8.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[284],{3766:function(e,r,n){Promise.resolve().then(n.bind(n,9361))},9361:function(e,r,n){"use strict";n.r(r);var o=n(7437),t=n(2265);function MyComponent(e){let{stringProps:r,booleanProps:n,numberProps:t}=e;return(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"My Component"}),(0,o.jsxs)("div",{children:["string props: ",r]}),(0,o.jsxs)("div",{children:["boolean props: ",String(n)]}),(0,o.jsxs)("div",{children:["number props: ",t]})]})}r.default=(0,t.memo)(MyComponent)},622:function(e,r,n){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var o=n(2265),t=Symbol.for("react.element"),s=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,u=o.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,n){var o,s={},f=null,l=null;for(o in void 0!==n&&(f=""+n),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(l=r.ref),r)i.call(r,o)&&!c.hasOwnProperty(o)&&(s[o]=r[o]);if(e&&e.defaultProps)for(o in r=e.defaultProps)void 0===s[o]&&(s[o]=r[o]);return{$$typeof:t,type:e,key:f,ref:l,props:s,_owner:u.current}}r.Fragment=s,r.jsx=q,r.jsxs=q},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=3766)}),_N_E=e.O()}]);
